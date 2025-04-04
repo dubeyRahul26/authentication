630050b0640cd05ee811c73a105656ffd88a6f99
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2025-04-04 11:28:37 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://authentication-system-q82x.onrender.com"],  // Allow both local and deployed frontend
+  origin: ["http://localhost:5173", "https://authentication-mbj2.onrender.com"],  // Allow both local and deployed frontend
   credentials: true
 }));
 
